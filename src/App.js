e20fa8f269a3f118ad43dfd3879929ea7105954c
salaam7860 <salaam7860@gmail.com>
2021-11-22 15:39:36 +0500

    setup Routing for the app.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import './App.css'
 import Homepage from "./pages/homepage/homepageComponent.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Router>
+        <Routes>
+          <Route  path='/' element={<Homepage />} />
+          <Route  path='/hats' element={<HatsPage />} />
+        
+        </Routes>
+      </Router>
     </div>
   );
 }
